8f74d796b267e43855c012619429027d9efd4215
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2023-12-18 16:12:43 +0300

    static files

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+  
+   Here, the assumption is that all saving accounts should have the same interest rate. Because static data
+is shared by all objects of the same category, if you were to change it in any way, all objects will “see” the
+new value the next time they access the static data, as they are all essentially looking at the same memory
+location. To understand how to change (or obtain) static data, you need to consider the role of static
+methods.
+
+
+*/
+using System;
+using StaticDataAndMembers;
+
+
+Console.WriteLine("****** Fun with Static Data *******");
+SavingsAccount s1 = new SavingsAccount(50);
+SavingsAccount s2 = new SavingsAccount(100);
+
+//Print the current interest rate
+Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+//Make new object, this oes NOT 'reset' the interest rate.
+SavingsAccount s3 = new SavingsAccount(10000.75);
+Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+Console.ReadLine();
---
+++ StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,21 @@
+using System;
+namespace StaticDataAndMembers
+{
+    
+   class SavingsAccount
+{
+ // Instance-level data.
+ public double currBalance;
+ // A static point of data.
+ public static double currInterestRate = 0.04;
+ public SavingsAccount(double balance)
+ {
+ currBalance = balance;
+ }
+ // Static members to get/set interest rate.
+ public static void SetInterestRate(double newRate)
+ => currInterestRate = newRate;
+ public static double GetInterestRate()
+ => currInterestRate;
+}
+}
